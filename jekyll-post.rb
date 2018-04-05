3c7f43f773931278f399496a2cc1c9bd422a2a7e
qqbuby <qqbuby@gmail.com>
2018-04-05 20:09:17 +0800

    Add jekyll-post.rb

---
+++ jekyll-post.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+# -*- encoding:utf-8 -*-
+#
+# The MIT License (MIT)
+# 
+# Copyright (c) 2018 ROY XU <qqbuby@gmail.com>
+#
+# Description: Create a jekyll post file named 'yyyy-MM-dd-title.md'.
+
+require 'optparse'
+require 'date'
+require 'securerandom'
+
+options = {}
+OptionParser.new do |opts|
+    opts.banner = "Usage: post.rb [options]"
+    opts.on('-t', '--title title', 'The title of the post') { |t| options[:title] = t }
+    opts.on('-c', '--category [\'category1\', \'category2\', .., \'categoryN\']', 'The category of the post') { |c| options[:category] = c }
+    opts.on('-f', '--filename filename', 'The file name of the post without date, default is \'--title\'') { |f| options[:filename] = f }
+    opts.on('--tag [\'tag1\', \'tag2\', .., \'tagN\']', 'The tags of the post') { |tag| options[:tag] = o }
+    opts.on('-h', '--help', 'Show this help message and exit')  do
+        puts opts
+        exit
+    end
+end.parse!
+
+raise "require the title of the post, '--title'" unless options[:title]
+raise "require the title of the post, '--category'" unless options[:category]
+
+title = options[:title]
+categories = options[:category]
+tags = options[:tag]
+unless tags 
+    tags = categories
+end
+
+filename = options[:filename]
+unless filename 
+    filename = title.dup
+end
+
+date = DateTime.now
+disqus_identifier = SecureRandom::uuid
+
+filename = filename.gsub!(' ', '-').downcase
+filename = date.strftime('%Y-%m-%d') + '-' + filename + ".md"
+date = date.strftime('%Y-%m-%d %H-%M-%S %z')
+
+front_matter = "---\nlayout: post\ntitle: #{title}\ndate: #{date}\ncategories: #{categories}\ntags: #{tags}\ndisqus_identifier: #{disqus_identifier}\n---"
+
+File.open(filename, 'w') { |f| f.write(front_matter) }
+
+__END__
