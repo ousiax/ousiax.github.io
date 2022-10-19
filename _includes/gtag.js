f0d2aed3b8222fd3c7543ab50eb1a5b6b093b348
XU <qqbuby@gmail.com>
2022-10-19 09:41:04 +0800

    Add _includes/gtag.js

---
+++ _includes/gtag.js
@@ -0,0 +1,11 @@
+{% if jekyll.environment == "production" %}
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SN88FJ18E5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SN88FJ18E5');
+</script>
+{% endif %}
